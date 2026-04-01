6df278c2363bc9477dd7d54226849888485eff5b
Nazareno Gutierrez <nazarenojunin@gmail.com>
2026-03-31 23:56:54 -0300

    Added svgs

---
+++ components/svg/MailSvg.tsx
@@ -0,0 +1,21 @@
+const MailSvg = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={className}
+      stroke="currentColor"
+      fill="none"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+      <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+    </svg>
+  );
+};
+
+export default MailSvg;
